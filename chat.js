060c82e3e80c8369dcccea9c4d2179cddfc92c60
= <=>
2016-02-04 19:55:31 +0100

    Mantener los sockets asociados a la lista de usuarios para poder enviar mensajes a un único usuario.

--- chat.js
+++ chat.js
@@ -1,55 +1,74 @@
-var _usuariosConectados = {};
+var _usuariosConectados = {
+//  socketID: {
+//      clientID: socketID
+//      nombre: '',
+//      ultimaConexion: new Date(),
+//      socket: socket
+//  }
+
+};
 var _nombreUsuarioPorDefecto = "Anonimo";
 var _sio;
 
 // Actualiza la lista de usuarios conectados y devuelve el objeto usuario correspondiente
 // al clientID.
-function nuevoUsuario(clientID) {
+function nuevoUsuario(socket) {
     'use strict';
-    _usuariosConectados[clientID] = {
+    var socketID = socket.client.conn.id;
+    _usuariosConectados[socketID] = {
         nombre: _nombreUsuarioPorDefecto,
-        ultimaConexion: new Date()
+        ultimaConexion: new Date(),
+        socket: socket,
+        clientID: socketID
     };
-    return _usuariosConectados[clientID];
+    return _usuariosConectados[socketID];
 }
 
-function conexionUsuario(clientID) {
+// Proporciona la lista de los usuarios conectados
+function usuarios() {
     'use strict';
-    var usuario = _usuariosConectados[clientID];
-    usuario.ultimaConexion = new Date();
-    return usuario;
+    var uu = [], usuario;
+    Object.keys(_usuariosConectados).forEach(function (id) {
+        usuario = _usuariosConectados[id];
+        uu.push({
+            nombre: usuario.nombre,
+            ultimaConexion: usuario.ultimaConexion
+        });
+    });
+    return uu;
 }
 
-function eventosEntrada(socket, clientID, usuario) {
+// Lista de los eventos que maneja el servicio.
+function eventosEntrada(usuario) {
     'use strict';
     return [
         {
             name: "disconnect",
             handler: function () {
-                console.log("Cliente desconectado:", clientID);
-                delete _usuariosConectados[clientID];
-                _sio.emit('online users', _usuariosConectados);
+                console.log("Cliente desconectado:", usuario.clientID);
+                delete _usuariosConectados[usuario.clientID];
+                _sio.emit('online users', usuarios());
             }
         },
         {
             name: 'chat message',
             handler: function (data) {
-                conexionUsuario(clientID);
+                usuario.ultimaConexion = new Date();
                 console.log('Nuevo mensaje: ' + data);
 
                 // socket.broadcast.emit envía el evento a todos los clientes menos al del socket.
                 // _sio.emit envía el evento a todos.
-                socket.broadcast.emit('chat message', usuario.nombre + " dice: " + data);
-                _sio.emit('online users', _usuariosConectados);
+                usuario.socket.broadcast.emit('chat message', usuario.nombre + " dice: " + data);
+                _sio.emit('online users', usuarios());
             }
         },
         {
             name: "new username",
             handler: function (data) {
                 console.log("Nuevo nombre de usuario:", data);
-                conexionUsuario(clientID);
+                usuario.ultimaConexion = new Date();
                 usuario.nombre = data;
-                _sio.emit('online users', _usuariosConectados);
+                _sio.emit('online users', usuarios());
             }
         }
     ];
@@ -59,20 +78,20 @@ function init(sio) {
     'use strict';
     _sio = sio;
     _sio.on("connection", function (socket) {
-        var clientID, usuario, eventos;
-
-        // Identificador del cliente
-        clientID = socket.client.conn.id;
+        var usuario, eventos;
 
         // Obtiene el usuario si estaba conectado o si no lo crea.
-        usuario = nuevoUsuario(clientID);
-        console.log("Cliente conectado:", clientID);
+        usuario = nuevoUsuario(socket);
+        console.log("Cliente conectado:", usuario.clientID);
+
         // Registrar los eventos del servidor socketio
-        eventos = eventosEntrada(socket, clientID, usuario);
+        eventos = eventosEntrada(usuario);
         eventos.forEach(function (ev) {
             socket.on(ev.name, ev.handler);
         });
-        _sio.emit('online users', _usuariosConectados);
+
+        // Enviar lista de usuarios conectados a todos los clientes.
+        _sio.emit('online users', usuarios());
     });
 }
 
